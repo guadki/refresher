674ab492f1de2ca98703ab06106bc3c9775ccf45
guadki <35728938+guadki@users.noreply.github.com>
2022-10-07 20:54:50 +0200

    Add structural types

---
+++ src/main/scala/other/TraitLimiting.scala
@@ -0,0 +1,22 @@
+package other
+
+object TraitLimiting {
+
+  class Fruit(name: String)
+
+  /**
+   * Only subclass of [[Fruit]] can have `trait` [[Juicy]] mixed into it
+   */
+  trait Juicy {
+    this: Fruit =>
+    def taste(): Unit
+  }
+
+  /**
+   * `Class` that wants to have `trait` [[Sour]] mixed into it must have `method` `taste()` defined
+   */
+  trait Sour[T] {
+    this: {def taste(bit: T): T} =>
+  }
+
+}
